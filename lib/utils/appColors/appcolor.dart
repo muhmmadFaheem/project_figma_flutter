55d65486f59239c76b913bd46b1f02960bd51918
muhmmadFaheem <www.faheem047@gmail.com>
2025-01-12 00:51:02 -0800

    first

---
+++ lib/utils/appColors/appcolor.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color titlewhite = Color(0xffFFFFFF);
+  static const Color titleblack = Color(0xff1A1A1A);
+  static const Color lightlime = Color(0xffd2ff1f);
+  static const Color darklime = Color(0xffabd600);
+  static const Color lightpurple = Color(0xffdcc1ff);
+  static const Color darkpurple = Color(0xffab93e0);
+  static const Color darkyellow = Color(0xfff7ce45);
+  static const Color lightyellow = Color(0xfff5f378);
+  static const Color lightorange = Color(0xffec704b);
+  static const Color darkorange = Color(0xffff4710);
+  static const Color lightbiscuit = Color(0xffFEF19D);
+  static const Color darkbiscuit = Color(0xffFFE078);
+  static const Color darkblue = Color(0xff0076FF);
+  static const Color darkgreen = Color(0xff3C9D00);
+  
+}
